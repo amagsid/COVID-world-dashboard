b93327498fde42f63e62e737efb6231a8c182dcf
Ahmad Magdy <amagsid@gmail.com>
2021-07-17 17:04:21 +0200

    fixed fa import

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,10 +1,12 @@
 import React from 'react';
+import { faShieldVirus } from '@fortawesome/free-solid-svg-icons';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 
 const Header = () => {
   return (
     <header>
       <div className={'header-container'}>
-        <i className="fas fa-shield-virus fa-fw"></i>
+        <FontAwesomeIcon icon={faShieldVirus} className="fas fa-shield-virus fa-fw"></FontAwesomeIcon>
 
         <h1>COVID-19 Worldwide Dashboard</h1>
       </div>

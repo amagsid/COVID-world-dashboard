67501f45a491a99c7059a6fe073358d0a0714474
Ahmad Magdy <amagsid@gmail.com>
2021-07-17 16:02:25 +0200

    changes for netlify

--- src/components/Header.js
+++ src/components/Header.js
@@ -6,7 +6,7 @@ const Header = () => {
       <div className={'header-container'}>
         <i className="fas fa-shield-virus fa-fw"></i>
 
-        <h1>COVID-19 Worldwide Dashboard</h1>
+        <h1>COVID-19 Worldwide Dashboardddd</h1>
       </div>
     </header>
   );
